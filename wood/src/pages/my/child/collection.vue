9b5334b0c33f4d2fe947f54bfd838eab3b025e57
hainanzhang <1379114708@qq.com>
2019-02-11 09:02:18 +0800

    zcg

--- wood/src/pages/my/child/collection.vue
+++ wood/src/pages/my/child/collection.vue
@@ -1,6 +1,28 @@
 <template>
 	<section class="collection">
 		<h1>wisspjksjk</h1>
+		<header class="header">
+			<van-icon name="close" />
+			<span>我的收藏</span>
+		</header>
+		<section class="main">
+			<nav class="mainNav">
+				<li>
+					<input type="radio" />
+					<section class="img">
+						<img src="" />
+					</section>
+					<section class="left">
+						<strong>
+							鹅软石沙发
+						</strong>
+						<span>
+							
+						</span>
+					</section>
+				</li>
+			</nav>
+		</section>
 	</section>
 </template>
 
